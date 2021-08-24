c03c7b4a0ff0cc7a7b940276e31bef7bc1f48352
unknown <joserodrigonmlopes@gmail.com>
2021-08-24 04:23:08 -0300

    Working with verbs

---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class personController : ControllerBase
+    {
+
+        private readonly ILogger<personController> _logger;
+
+        public personController(ILogger<personController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet ("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            
+        }
+
+    }
+}
